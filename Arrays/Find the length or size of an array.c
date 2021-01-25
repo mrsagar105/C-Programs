effbe46ad81b081265489e7452e41478f824f981
Sagar Biswas <mrsagar105@gmail.com>
2021-01-25 06:13:52 +0530

    Second program

---
+++ Arrays/Find the length or size of an array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[6] = {1, 2, 3, 4, 5, 6};
+
+    // as array 'a' is of integer type
+    printf("sizeof a[0]: %d\n", sizeof(a[0])); // 4
+    printf("sizeof a[1]: %d\n", sizeof(a[1])); // 4
+    printf("sizeof a[2]: %d\n", sizeof(a[2])); // 4
+
+    // as array 'a' has 6 integer elements so 6 * 4 = 24
+    printf("sizeof a: %d\n", sizeof(a)); // 24
+
+    // Finds the length of array
+    printf("Length of array: %d\n", sizeof(a)/sizeof(a[0])); // 6
+
+    return 0;
+}
