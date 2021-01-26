122f15215870785fae1a9b5b0a34de326cadaddc
Sagar Biswas <mrsagar105@gmail.com>
2021-01-27 03:36:27 +0530

    Create Taking input from user and storing it inside an array.c

---
+++ Arrays/Taking input from user and storing it inside an array.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[10];
+
+    // takes 5 values from the user
+    // and stores them inside array 'a' from a[0] to a[4]
+    for(int i = 0; i <= 4; i++) {
+        scanf("%d", a[i]);
+    }
+
+    return 0;
+}
