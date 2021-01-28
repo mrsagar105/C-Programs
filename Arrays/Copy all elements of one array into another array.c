d724cf2f0f2aadb54499b3fc4a163af9a011460a
Sagar Biswas <mrsagar105@gmail.com>
2021-01-28 05:59:43 +0530

    Update and rename copy of duplicate elements of one array into another array.c to Copy all elements of one array into another array.c

---
+++ Arrays/Copy all elements of one array into another array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int array1[] = {1, 2, 3, 4, 5};
+
+    int length = sizeof(array1)/sizeof(array1[0]); // calculates the size af first array
+    int array2[length]; // declaring second array of same size
+
+    // Copy elements of first array into second array
+    for(int i = 0; i < length; i++) {
+        array2[i] = array1[i];
+    }
+
+    // Prints the elements of first array
+    printf("Elements in array1: ");
+    for(int i = 0; i < length; i++) {
+        printf("%d ", array1[i]);
+    }
+
+    // Prints the elements of second array
+    printf("\nElements in array2: ");
+    for(int i = 0; i < length; i++) {
+        printf("%d ", array2[i]);
+    }
+
+    return 0;
+}
