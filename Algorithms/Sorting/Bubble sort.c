4c2137d6187818769cf93428383e575778b60463
Sagar Biswas <mrsagar105@gmail.com>
2021-02-08 02:58:45 +0530

    Update Bubble sort.c

--- Algorithms/Sorting/Bubble sort.c
+++ Algorithms/Sorting/Bubble sort.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 // bubble sort function
-int bubbleSort(int *array, int size)
+int bubbleSort(int *a, int size)
 {
     int temp;
 
     for(int i = 0; i < size-1; i++) {
         for(int j = 0; j < size-1-i; j++) {
-            if(array[j] > array[j + 1]) {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
+            if(a[j] > a[j + 1]) {
+                temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
             }
         }
     }
@@ -18,7 +18,7 @@ int bubbleSort(int *array, int size)
 
 int main()
 {
-    int array[100], size;
+    int a[100], size;
 
     // storing number of elements in variable size 
     printf("Enter number of elements: ");
@@ -27,22 +27,22 @@ int main()
     // taking input in array
     for(int i = 0; i < size; i++) {
         printf("Enter Element %d: ", i + 1);
-        scanf("%d", &array[i]);
+        scanf("%d", &a[i]);
     }
 
     // printing unsorted array
     printf("\nUnsorted array: ");
     for(int i = 0; i < size; i++) {
-        printf("%d ", array[i]);
+        printf("%d ", a[i]);
     }
     
     // function for sorting array
-    bubbleSort(array, size);
+    bubbleSort(a, size);
 
     // printing sorted array
     printf("\nSorted array: ");
     for(int i = 0; i < size; i++) {
-        printf("%d ", array[i]);
+        printf("%d ", a[i]);
     }
 
     return 0;
