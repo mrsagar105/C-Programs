fcaae22f0e7ca19f637b8929f080b7b0bc139e1f
Sagar Biswas <mrsagar105@gmail.com>
2021-02-08 03:00:19 +0530

    Update Selection sort.c

--- Algorithms/Sorting/Selection sort.c
+++ Algorithms/Sorting/Selection sort.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 // selection sort function
-int selectionSort(int *array, int size)
+int selectionSort(int *a, int size)
 {
     int temp;
 
     for(int i = 0; i < size; i++) {
         for(int j = i + 1; j < size; j++) {
-            if(array[j] < array[i]) {
-                temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
+            if(a[j] < a[i]) {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
             }
         }
     }
@@ -18,7 +18,7 @@ int selectionSort(int *array, int size)
 
 int main()
 {
-    int array[100], size;
+    int a[100], size;
 
     // storing number of elements in variable size 
     printf("Enter number of elements: ");
@@ -27,22 +27,22 @@ int main()
     // taking input in array
     for(int i = 0; i < size; i++) {
         printf("Enter Element %d: ", i + 1);
-        scanf("%d", &array[i]);
+        scanf("%d", &a[i]);
     }
 
     // printing unsorted array
     printf("\nUnsorted array: ");
     for(int i = 0; i < size; i++) {
-        printf("%d ", array[i]);
+        printf("%d ", a[i]);
     }
     
     // function for sorting array
-    selectionSort(array, size);
+    selectionSort(a, size);
 
     // printing sorted array
     printf("\nSorted array: ");
     for(int i = 0; i < size; i++) {
-        printf("%d ", array[i]);
+        printf("%d ", a[i]);
     }
 
     return 0;
