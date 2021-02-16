24649779a8cee0eb4a334f44c49a078f4226c903
Sagar Biswas <mrsagar105@gmail.com>
2021-02-17 02:27:30 +0530

    Update Printing string of character array.c

--- Strings/Printing string of character array.c
+++ Strings/Printing string of character array.c
@@ -4,18 +4,24 @@ int main()
 {
     char a[] = "Hello";
 
-    /* points to the 1st character of string
-       prints until it reaches the NULL character '\n' */
+    // points to the 1st character of string
+    prints until it reaches the NULL character '\n'
     printf("%s\n", a); // Hello
 
-    /* points to the 1st character of string + 1 i.e 2nd */
+    // points to the 1st character of string + 1 i.e 2nd
     printf("%s\n", a+1); // ello
 
-    /* points to the 1st character of string + 2 i.e 3rd */
+    // points to the 1st character of string + 2 i.e 3rd
     printf("%s\n", a+2); // llo
 
-    /* prints the first 3 characters */
+    // prints the first 3 characters
     printf("%.3s", a); // Hel
+
+    // puts()
+    // Same as printf() function.
+    // Does not require %s.
+    // Automatically adds a new line character.
+    puts(a); // Hello
 
     return 0;
 }
